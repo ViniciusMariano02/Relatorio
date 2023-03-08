c0a36474a9c2d7ab91d605aad695cb70dd0e680d
Vinicius Mariano <93166937+ViniciusMariano02@users.noreply.github.com>
2023-03-08 16:58:16 -0300

    V. PRE ALPHA 0.0.2

---
+++ src/components/modalFiliais/index.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import React from "react";
+
+
+
+const Modal = props => {
+
+    return(
+        <div>
+
+            <p>Meu modal esta funcionando com sucesso!</p>
+        </div>
+    )
+}
+
+export default Modal;
